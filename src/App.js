fde1d33fe34193782ace213240ccf82248e6707f
AnujaOswal <Oswalanujak@gmail.com>
2021-09-29 22:56:08 +0530

    inital commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Header from './components/Header.js'
+import Balance from './components/Balance.js';
+import IncomeExpenses from './components/IncomeExpenses.js';
+import TranscationList from './components/TranscationList.js';
+import {AddTransaction} from './components/AddTransaction.js';
+import './App.css';
+import {GlobalProvider} from './context/GlobalState';
+
+
+function App() {
+  return (
+    <div>
+    <GlobalProvider/>
+      <Header/>
+      <div className="conatiner">
+        {/* displaying balance */}
+        <Balance/>
+        {/* showing income and expense */}
+        <IncomeExpenses/>
+        {/* showing transcation history done before(transcation track) */}
+        <TranscationList/>
+        {/* adding new transcation expense */}
+        <AddTransaction />
+      </div>
+      <GlobalProvider/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <h2>
+            EXPENSE TRACKE₹
+        </h2>
+    )
+}
+
+export default Header
